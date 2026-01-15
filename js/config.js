c96139f932f91353f8bf1872bfa9c9bd61adf5b2
huycozy <huy@nevercode.io>
2026-01-15 11:03:15 +0700

    update store url

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    APP_STORE_URL: 'https://apps.apple.com/app-id-placeholder',
-    PLAY_STORE_URL: 'https://play.google.com/store/apps/details?id=com.example.phatnguoi',
+    APP_STORE_URL: 'https://apps.apple.com/vn/app/id6757685023',
+    PLAY_STORE_URL: 'https://play.google.com/store/apps/details?id=com.app.phatnguoionline',
 };
